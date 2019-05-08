909afa206ff3b94c575e440e9338d02437d2fe54
250747293 <250747293@qq.com>
2019-05-08 09:34:27 +0800

    Merge branches 'dev_zyh' and 'wyx' of https://github.com/baiyajin/cloud into wyx
    # Conflicts:
    #	.idea/modules.xml
    #	.idea/workspace.xml

---
+++ entity/src/main/java/com/baiyajin/entity/bean/Page.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.baiyajin.entity.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 分页类
+ * @author ThinkGem
+ * @version 2013-7-2
+ * @param <T>
+ */
+public class Page<T> {
+	private int pageSize;
+	private int pageNo;
+	private int count;
+	private List<T> list = new ArrayList<T>();
+
+	public Page() {
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public List<T> getList() {
+		return list;
+	}
+
+	public void setList(List<T> list) {
+		this.list = list;
+	}
+}
