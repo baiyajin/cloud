4e8edb4b38440f46ff91d490d98718591b1c0d8b
250747293 <250747293@qq.com>
2019-05-10 16:24:31 +0800

    材料统计

--- materials/src/main/java/com/baiyajin/materials/controller/PageMaterialController.java
+++ materials/src/main/java/com/baiyajin/materials/controller/PageMaterialController.java
@@ -6,13 +6,13 @@ import com.baiyajin.entity.bean.MaterialCount;
 import com.baiyajin.entity.bean.MaterialVo;
 import com.baiyajin.entity.bean.PageMaterial;
 import com.baiyajin.materials.service.PageMaterialInterface;
-import com.baiyajin.util.u.IdGenerate;
-import com.baiyajin.util.u.Results;
-import com.baiyajin.util.u.ReturnModel;
+import com.baiyajin.util.u.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.apache.shiro.crypto.hash.Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,9 +25,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Timestamp;
 import java.text.ParseException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Api("材料")
 @Controller
@@ -138,10 +137,9 @@ public class PageMaterialController {
     @Transactional(rollbackFor = Exception.class)
     @ResponseBody
     public List<Map<String,Object>> getMaterialsInfoByRecent(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String,Object> map) throws ParseException {
-
-
         map.put("level","1");
-        map.put("type",0);
+        map.put("type","0");
+        map.put("area","53");
         map.put("startDate","2019-03");
         map.put("endDate","2019-03");
       return pageMaterialInterface.getMaterialsInfo(map);
@@ -163,18 +161,58 @@ public class PageMaterialController {
 
     }
 
+    @ApiOperation(value = "",notes = "json")
+    @ApiImplicitParams({@ApiImplicitParam(name = "",value =  "",dataType = "String")})
+    @RequestMapping(value = "/getMaterialsInfoByAreaEncapsulation", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
+    @Transactional(rollbackFor = Exception.class)
+    @ResponseBody
+    public ReturnModel getMaterialsInfoByAreaEncapsulation(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String,Object> map) throws ParseException {
 
+
+
+        map.put("type","0");
+
+        List<Map<String,Object>> list = pageMaterialInterface.getMaterialsInfoByArea(map);
+        return new ReturnModel( 1,listToTreeArea(list));
+
+    }
+
+    public   Map<String,List<Map<String,Object>>> listToTreeArea( List<Map<String,Object>> childrenMaterialsList ){
+        Map<String,List<Map<String,Object>>> map = new HashMap<>();
+        System.out.println("size"+childrenMaterialsList.size());
+        for(Map<String,Object> ml:childrenMaterialsList){
+            if( map.get(ml.get("area").toString())!=null){
+                List<Map<String,Object>> list =  map.get(ml.get("area").toString());
+                list.add(ml);
+                map.put(ml.get("area").toString(),list);
+            }else{
+                List<Map<String,Object>> list =  new ArrayList<>();
+                list.add(ml);
+                map.put(ml.get("area").toString(),list);
+            }
+        }
+        return map;
+    }
+
+
+
+
+    /**
+     * 获取材料统计数量
+     * @param map
+     * @return
+     */
     @RequestMapping(value = "/getMaterialCount", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
     @ResponseBody
     public ReturnModel getMaterialCount( @RequestBody Map<String, Object> map) {
         Map<String, Object> reMap = new HashMap<>();
         try {
-            map.put("type", 0);
+            map.put("area", "530100000000");
             MaterialCount kmMaterialCount = pageMaterialInterface.getMaterialCount(map);
-            map.put("type", 1);
+            map.remove("area");
+
             MaterialCount otherMaterialCount = pageMaterialInterface.getMaterialCount(map);
-            map.remove("type");
-            map.put("area", "53");
+            otherMaterialCount.setCount(otherMaterialCount.getCount()-kmMaterialCount.getCount());
             map.put("mid", 0);
             MaterialCount materialCountAll = pageMaterialInterface.getMaterialCount(map);
             reMap.put("km", kmMaterialCount);
@@ -183,11 +221,170 @@ public class PageMaterialController {
         }catch (Exception e){
             return new ReturnModel(0,null);
         }
-        ReturnModel aaa =new ReturnModel(1,reMap);
-        System.out.println(aaa);
-        return aaa;
+
+        return new ReturnModel(1,reMap);
 
     }
+
+    /**
+     * 获取市级区域信息
+     * @param map
+     * @return
+     * @throws ParseException
+     */
+    @RequestMapping(value = "/getMaterialsInfoByAllCities", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public ReturnModel getMaterialsInfoByAllCities( @RequestBody Map<String, Object> map) throws ParseException {
+
+    Integer monthNumber = Integer.parseInt(map.get("monthNumber").toString())-1;
+
+        map.put("level","1,2,3");
+        map.put("type","0");
+        if(map.get("area")==null){
+            String areas = "530100000000,530300000000,530400000000,530500000000,530600000000,530700000000," +
+                    "530800000000,530900000000,532300000000,532500000000,532600000000,532800000000,532900000000,533100000000,533300000000,533400000000";
+            map.put("area",areas);
+        }
+
+        Date date = DateFormatUtil.stringToDate("2019-03-31 23:59:59");
+        Date stDate = DateFormatUtil.dateCompute(date,2,monthNumber * -1);
+        stDate = DateFormatUtil.setDate(stDate,5,1);
+        map.put("startDate",DateFormatUtil.dateToString(stDate,"yyyy-MM"));
+        map.put("endDate",DateFormatUtil.dateToString(date,"yyyy-MM"));
+System.out.println(map);
+        List<Map<String,Object>> materialsList = pageMaterialInterface.getMaterialsInfo(map);
+
+       // pageMaterialInterface
+        return new ReturnModel(1,materialsList);
+    }
+
+
+
+
+
+    /**
+     * 获取材料封装
+     * @param map
+     * @return
+             * @throws ParseException
+     */
+    @RequestMapping(value = "/getMaterialsInfoEncapsulation", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public ReturnModel getMaterialsInfoEncapsulation( @RequestBody Map<String, Object> map) throws ParseException {
+
+        map.put("level","1,2,3");
+        map.put("area","53");
+        map.put("type","0");
+        String pid = map.get("pid")==null?null:map.get("pid").toString();
+        List<Map<String,Object>> childrenMaterialsList = pageMaterialInterface.getMaterialsInfo(map);
+
+
+
+
+//        map.remove("pid");
+//        map.put("id",pid);
+//        List<Map<String,Object>> materialsList = pageMaterialInterface.getMaterialsInfo(map);
+
+        //Map<String, List<String>> resultList = materialsList.stream().collect(Collectors.groupingBy(String::get("level").toString()));
+//        Map<String, List<Map<String,Object>>> remap = ListToTree(materialsList,childrenMaterialsList);
+        return new ReturnModel(1,ListToTree(childrenMaterialsList));
+    }
+
+
+
+    public   Map<String,List<Map<String,Object>>> ListToTree( List<Map<String,Object>> childrenMaterialsList ){
+        Map<String,List<Map<String,Object>>> map = new HashMap<>();
+       for(Map<String,Object> ml:childrenMaterialsList){
+          if( map.get(ml.get("mid").toString())!=null){
+              List<Map<String,Object>> list =  map.get(ml.get("mid").toString());
+              list.add(ml);
+              map.put(ml.get("mid").toString(),list);
+          }else{
+              List<Map<String,Object>> list =  new ArrayList<>();
+              list.add(ml);
+              map.put(ml.get("mid").toString(),list);
+          }
+       }
+        return map;
+    }
+
+
+
+//
+//
+//    public Map<String, List<Map<String,Object>>> ListToTree(List<Map<String,Object>> materialsList,List<Map<String,Object>> cmaterialsList) throws ParseException {
+//
+//        List<Map<String,Object>> list1 = new ArrayList<Map<String,Object>>();
+//
+//        Map<String, List<Map<String,Object>>> map = new HashMap<>();
+//
+//        if(materialsList.size()==0){
+//            map.put("0",cmaterialsList);
+//            return map;
+//        }
+//        for(Map<String,Object> m:materialsList){
+//            m.put("children",new ArrayList<Map<String,Object>>());
+//        }
+//
+////        for(Map<String,Object> m:materialsList ){
+////            if(map.get(m.get("mid").toString())==null){
+////                List<Map<String,Object>> list11 = new ArrayList<Map<String,Object>>();
+////                list11.add(m);
+////                map.put(m.get("mid").toString(),list11);
+////            }else{
+////                List<Map<String,Object>> list11 = (List<Map<String, Object>>) map.get(m.get("mid").toString());
+////                list11.add(m);
+////                map.put(m.get("mid").toString(),list11);
+////            }
+////        }
+//
+//
+//
+//       for(Map<String,Object> m:materialsList) {
+//           for (Map<String, Object> cm : cmaterialsList) {
+//               System.out.println("pid:" + cm.get("pid"));
+//               System.out.println("mid:" + m.get("mid"));
+//
+//              Date a = DateFormatUtil.stringToDate(cm.get("asmdate").toString());
+//               Calendar cale = Calendar.getInstance();
+//               cale.setTime(a);
+//               int month = cale.get(Calendar.MONTH) + 1;
+//               Date a1 = DateFormatUtil.stringToDate(m.get("asmdate").toString());
+//               Calendar cale1 = Calendar.getInstance();
+//               cale1.setTime(a1);
+//               int month1 = cale1.get(Calendar.MONTH) + 1;
+//
+//               if (cm.get("pid").toString().equals(m.get("mid").toString()) && month==month1) {
+//                   List<Map<String, Object>> list = (List<Map<String, Object>>) m.get("children");
+//                   list.add(cm);
+//                   m.put("children", list);
+//                   list1.add(m);
+//               }
+//           }
+//       }
+//
+//
+//        for(Map<String,Object> m:list1 ){
+//            if(map.get(m.get("mid").toString())==null){
+//                List<Map<String,Object>> list11 = new ArrayList<Map<String,Object>>();
+//                list11.add(m);
+//                map.put(m.get("mid").toString(),list11);
+//            }else{
+//                List<Map<String,Object>> list11 = (List<Map<String, Object>>) map.get(m.get("mid").toString());
+//                list11.add(m);
+//                map.put(m.get("mid").toString(),list11);
+//            }
+//        }
+//
+//
+//
+//
+//        return  map;
+//    }
+//
+
+
+
 
 
 
