c7e838975e42fda276bb8e2dd8cdf783c26713ce
250747293 <250747293@qq.com>
2019-05-13 09:52:44 +0800

    材料统计

--- materials/src/main/java/com/baiyajin/materials/controller/PageAreaController.java
+++ materials/src/main/java/com/baiyajin/materials/controller/PageAreaController.java
@@ -29,35 +29,35 @@ public class PageAreaController {
 
 
 
-    /**
-     * 添加地址
-     * @param request
-     * @param response
-     * @param map
-     * @return
-     */
-    @RequestMapping(value = "/addArea", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
-    @Transactional(rollbackFor = Exception.class)
-    @ResponseBody
-    public Map<String,Object> addArea(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String,Object> map) {
-        Map<String,Object> reMap = new HashMap<String,Object>();
-        try{
-            int i = pageAreaInterface.save(map);
-            if(i==0){
-                reMap.put("stat",false);
-                reMap.put("msg","操作失败");
-            }else{
-                reMap.put("stat",true);
-                reMap.put("msg","操作成功");
-            }
-            return reMap;
-        }catch (Exception e){
-            e.printStackTrace();
-            reMap.put("stat",false);
-            reMap.put("msg","操作失败");
-            return reMap;
-        }
-    }
+//    /**
+//     * 添加地址
+//     * @param request
+//     * @param response
+//     * @param map
+//     * @return
+//     */
+//    @RequestMapping(value = "/addArea", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
+//    @Transactional(rollbackFor = Exception.class)
+//    @ResponseBody
+//    public Map<String,Object> addArea(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String,Object> map) {
+//        Map<String,Object> reMap = new HashMap<String,Object>();
+//        try{
+//            int i = pageAreaInterface.save(map);
+//            if(i==0){
+//                reMap.put("stat",false);
+//                reMap.put("msg","操作失败");
+//            }else{
+//                reMap.put("stat",true);
+//                reMap.put("msg","操作成功");
+//            }
+//            return reMap;
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            reMap.put("stat",false);
+//            reMap.put("msg","操作失败");
+//            return reMap;
+//        }
+//    }
 
 
     @RequestMapping(value = "/getAreaList", method = {RequestMethod.POST}, produces = "application/json;charset=UTF-8")
